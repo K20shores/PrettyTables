f316ddbd47fd5490ff6c131a4a04ba8258bb8954
Kyle Shores <kyle.shores44@gmail.com>
2021-05-08 11:31:01 -0500

    setting up project structure

---
+++ pretty_table/table.py
@@ -0,0 +1,43 @@
+from matplotlib.artist import Artist
+
+
+class Table(Artist):
+    """
+    A table used to represent tabular information
+
+
+    Attributes
+    ----------
+    """
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def __setitem__(self):
+        pass
+
+    def merge_cells(self):
+        pass
+
+
+class Cell():
+    """
+    """
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
